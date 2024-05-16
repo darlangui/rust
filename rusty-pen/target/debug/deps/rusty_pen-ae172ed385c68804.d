630c341a8ccb4ba52e280b775d3e76b8fc96b5c7
Darlan <darlaneduardoguimaraes@gmail.com>
2024-05-16 14:41:55 -0300

    init project

---
+++ rusty-pen/target/debug/deps/rusty_pen-ae172ed385c68804.d
@@ -0,0 +1,5 @@
+C:\Users\darla\RustroverProjects\rusty-pen\target\debug\deps\librusty_pen-ae172ed385c68804.rmeta: src\main.rs
+
+C:\Users\darla\RustroverProjects\rusty-pen\target\debug\deps\rusty_pen-ae172ed385c68804.d: src\main.rs
+
+src\main.rs:
